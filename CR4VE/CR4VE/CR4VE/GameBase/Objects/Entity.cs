1996bb30cbe6a6ccb4394a7b9c5e386c52db130e
Dr-Zock- <gorky8@web.de>
2014-05-23 23:25:00 +0200

    Entity.cs

---
+++ CR4VE/CR4VE/CR4VE/GameBase/Objects/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CR4VE.GameBase.Objects
+{
+    public class Entity
+    {
+        private Vector3 position;
+        private static Model model;
+        private static BoundingBox boundary;
+    }
+}
