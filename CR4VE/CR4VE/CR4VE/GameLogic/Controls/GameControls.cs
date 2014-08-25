fda801e8b79b29b585d5acaf95443adac41e0521
Dr-Zock- <gorky8@web.de>
2014-08-25 20:42:04 +0200

    Sprung verbessert (immer die selbe Sprunghoehe)

--- CR4VE/CR4VE/CR4VE/GameLogic/Controls/GameControls.cs
+++ CR4VE/CR4VE/CR4VE/GameLogic/Controls/GameControls.cs
@@ -20,16 +20,18 @@ namespace CR4VE.GameLogic.Controls
     public static class GameControls
     {
         #region Attributes
-        //Keyboard- und Mausparameter
+        //Keyboard-, Maus- und Gamepadparameter
         static KeyboardState currentKeyboard;
         static KeyboardState previousKeyboard;
+
         static MouseState currentMouseState;
         static MouseState previousMouseState;
+
         static GamePadState currGamepad;
         static GamePadState prevGamepad;
 
         //Bewegungsparameter
-        private static readonly float accel = 1;
+        private static readonly float accel = 1f;
 
         public static bool borderedLeft = false;
         public static bool borderedRight = false;
@@ -37,9 +39,8 @@ namespace CR4VE.GameLogic.Controls
         public static bool borderedBottom = false;
 
         //Sprungparameter
-        private static readonly float G = 9.81f;
-        private static readonly float velocityGain = 0.25f;
-        private static readonly float maxVelocity = 2.5f;
+        private static readonly float G = 9.5f;
+        private static readonly float jumpHeight = 2.5f;
 
         private static double startFallTime;
         private static double startJumpTime;
@@ -48,9 +49,6 @@ namespace CR4VE.GameLogic.Controls
         public static bool isJumping = false;
         public static bool isAirborne = true;
         public static bool isFalling = true;
-        
-        private static float velocityLeft = 0;
-        private static float velocityRight = 0;
         #endregion
 
         #region Methods
@@ -119,31 +117,23 @@ namespace CR4VE.GameLogic.Controls
             #region left and right movement
             if ((currentKeyboard.IsKeyDown(Keys.A) || currGamepad.IsButtonDown(Buttons.DPadLeft)) && !borderedLeft)
             {
-                if (velocityLeft < maxVelocity) velocityLeft += velocityGain;
-                velocityRight = 0;
                 moveVecPlayer += new Vector3(-accel, 0, 0);
 
                 borderedRight = false;
 
-                //richtung = links
+                //Blickrichtung = links
                 Singleplayer.player.viewingDirection.X = -1;
             }
-            else
-                velocityLeft = MathHelper.Clamp(velocityLeft -= velocityGain, 0, maxVelocity);
 
             if ((currentKeyboard.IsKeyDown(Keys.D) || currGamepad.IsButtonDown(Buttons.DPadRight)) && !borderedRight)
             {
-                if (velocityRight < maxVelocity) velocityRight += velocityGain;
-                velocityLeft = 0;
                 moveVecPlayer += new Vector3(accel, 0, 0);
 
                 borderedLeft = false;
 
-                //richtung = rechts
+                //Blickrichtung = rechts
                 Singleplayer.player.viewingDirection.X = 1;
             }
-            else
-                velocityRight = MathHelper.Clamp(velocityRight -= velocityGain, 0, maxVelocity);
             #endregion
 
             #region airborne influence
@@ -152,13 +142,6 @@ namespace CR4VE.GameLogic.Controls
             {
                 isJumping = true;
                 borderedBottom = false;
-
-                //minimum jump
-                if (velocityLeft == 0f && velocityRight == 0f)
-                {
-                    velocityLeft = 2.5f;
-                    velocityRight = 2.5f;
-                }
 
                 startJumpTime = gameTime.TotalGameTime.TotalSeconds;
             }
@@ -186,7 +169,7 @@ namespace CR4VE.GameLogic.Controls
                 currentTime = gameTime.TotalGameTime.TotalSeconds;
 
                 double deltaTime = currentTime - startJumpTime;
-                moveVecPlayer += new Vector3(0, Math.Max(velocityRight, velocityLeft) - (float)deltaTime * G, 0);
+                moveVecPlayer += new Vector3(0, jumpHeight - (float)deltaTime * G, 0);
             }
             #endregion
             
